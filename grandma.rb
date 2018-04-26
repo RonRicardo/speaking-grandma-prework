11c3c7876aee8135036d2dce823327552f9649ba
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-26 21:07:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,5 +18,6 @@ def speak_to_grandma(words)
   elsif words == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN'
   else
-    return 'HUH?! SPEAK UP, SONNY!' 
+    return 'HUH?! SPEAK UP, SONNY!'
+  end
 end
