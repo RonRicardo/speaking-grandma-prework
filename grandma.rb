323833c7fa46ed613a50baba9fd1c1b08131a5c2
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-26 21:02:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(words)
   if words == words.downcase
-    return 'HUH?! SPEAK UP, SONNY!'
+    puts 'HUH?! SPEAK UP, SONNY!'
   elsif words == 'I LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
+    puts 'I LOVE YOU TOO PUMPKIN!'
   else
-    return 'NO, NOT SINCE 1938!'
+    puts 'NO, NOT SINCE 1938!'
 end
